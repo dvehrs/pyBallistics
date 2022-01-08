cc861de69335adbe0cbd83f2ec90cba1c22eca02
Dave Vehrs <dvehrs@gmail.com>
2022-01-08 15:20:50 -0700

    Added configuration dictionary

--- bdc.py
+++ bdc.py
@@ -10,6 +10,9 @@ def calcBDC(range_max = constants.BALLISTICS_COMPUTATION_MAX_YARDS, bc = 0.269, 
 
     logger = logging.getLogger()
 
+    # Declare empty dictionary to hold configuration settings
+    configuration = {}
+
     k = 0
     # The wind speed in miles per hour.
     windspeed = 0
@@ -29,11 +32,21 @@ def calcBDC(range_max = constants.BALLISTICS_COMPUTATION_MAX_YARDS, bc = 0.269, 
     # Correct the BC for any weather conditions.  If we want standard conditions,
     # then we can just leave this commented out.
 
+    configuration['range max'] = range_max
+    configuration['bc input'] = bc
+    configuration['velocity'] = v
+    configuration['site height'] = sh
+    configuration['angle - shooting'] = angle
+    configuration['zero distance'] = zero_dist
+    configuration['zero unit'] = zero_unit
+
     bc = atmosphere.atmosphere_correction(
         bc, altitude, barometer, temperature, relative_humidity)
 
     logger.info("bc {}".format(bc))
     # print("bc {}".format(bc))
+
+    configuration['bc corrected'] = bc
 
     # Convert zero range in meters to yards for calculating the zero angle
     if zero_unit.lower() == 'm':
@@ -48,10 +61,12 @@ def calcBDC(range_max = constants.BALLISTICS_COMPUTATION_MAX_YARDS, bc = 0.269, 
     # final solution, or to allow for zero's other than 0" (ex: 3" high at 100 yds)
     zeroangle = angles.zero_angle(drag_function, bc, v, sh, zero_dist, 0)
 
+    configuration['angle - bore'] = zeroangle
+
     # Now we have everything needed to generate a full solution.
     # So we do.  The solution is stored in the pointer "sln" passed as the last argument.
     # k has the number of yards the solution is valid for, also the number of rows in the solution.
     hold_overs = ballistics.solve(range_max, drag_function, bc, v, sh, angle,
                                   zeroangle, windspeed, windangle)
 
-    return hold_overs
+    return configuration, hold_overs
--- example_rich.py
+++ example_rich.py
@@ -24,60 +24,57 @@ logging.info('Example Info Message')
 logging.warning('Example Warning Message')
 
 #hold_overs = calcBDC(400)
-#hold_overs = calcBDC(range = 400, zero_unit = "m")
+#configuration, hold_overs = calcBDC(range_max = 400, zero_unit = "m")
 #hold_overs = calcBDC(zero_unit = "m", range = 400)
-hold_overs = calcBDC()
+configuration, hold_overs = calcBDC(range_max = 100)
+#configuration, hold_overs = calcBDC()
 
-#print()
-#print("All Points in hold_overs:")
-#for point in hold_overs.points:
-#    incline_compensation = get_incline_compensation(point.path_inches, -15)
-#
-#    cant_compensation = get_cant_compensation(point.seconds, 90, 1.5)
-#
-#    print("hold_over %8s %8s %s %s %s %s" %
-#          (point.yards, point.meters, point.path_inches, incline_compensation, abs(point.path_inches-incline_compensation), cant_compensation))
+console = Console()
 
-#table = Table(title="All Points in hold_overs")
-#table.add_column("Yards", justify="right", no_wrap=True)
-#table.add_column("Meters", justify="right", no_wrap=True)
-#table.add_column("Elevation - Inches", justify="right", no_wrap=True)
-#table.add_column("Incline Compensation", justify="right", no_wrap=True)
-#table.add_column("Inches-Incline", justify="right", no_wrap=True)
-#table.add_column("Cant Compensation", justify="right", no_wrap=True)
-#for point in hold_overs.points:
-#    incline_compensation = get_incline_compensation(point.path_inches, -15)
-#
-#    cant_compensation = get_cant_compensation(point.seconds, 90, 1.5)
-#
-##    print("hold_over %8s %8s %s %s %s %s" %
-##          (point.yards, point.meters, point.path_inches, incline_compensation, abs(point.path_inches-incline_compensation), cant_compensation))
-#    table.add_row (point.yards, point.meters, point.path_inches, incline_compensation, abs(point.path_inches-incline_compensation), cant_compensation)
+# Print Configuration Table
+conftable = Table(title="Configuration")
+conftable.add_column("Key", justify="left", no_wrap=True)
+conftable.add_column("Value", justify="left", no_wrap=True)
+for item in sorted(configuration):
+    conftable.add_row(item, str(configuration[item]))
 
+console.print(conftable)
+print()
 
-#print()
-#print("Whole Yards Only")
-#print("%-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
-#      ("Range", "Drop", "MOA", "Mils", "Time", "Velocity"))
-#print("%-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
-#      ("(Yards)", "(Inches)", "", "", "(Seconds)", "(fps)"))
-#for point in hold_overs.points:
-#    if float(point.yards).is_integer():
-#        pi = '{:.3f}'.format(round(point.path_inches, 3))
-#        moac = '{:.2f}'.format(round(point.moa_correction, 2))
-#        milc = '{:.2f}'.format(round(point.mil_correction, 2))
-#        stime = '{:.3f}'.format(round(point.seconds, 3))
-#        vel = '{:.2f}'.format(round(point.velocity, 2))
-#        print("%8s | %8s  | %8s | %8s | %9s | %8s |" %
-#              (point.yards, pi, moac, milc, stime, vel))
-#
-table = Table(title="Whole Yards Only")
-table.add_column("Yards", justify="right", no_wrap=True)
-table.add_column("Drop - Inches", justify="right", no_wrap=True)
-table.add_column("Drop - MOA", justify="right", no_wrap=True)
-table.add_column("Drop - Mils", justify="right", no_wrap=True)
-table.add_column("Time", justify="right", no_wrap=True)
-table.add_column("Velocity", justify="right", no_wrap=True)
+alltable = Table(title="All Points in hold_overs")
+alltable.add_column("Yards", justify="right", no_wrap=True)
+alltable.add_column("Meters", justify="right", no_wrap=True)
+alltable.add_column("Elevation - Inches", justify="right", no_wrap=True)
+alltable.add_column("Incline Compensation", justify="right", no_wrap=True)
+alltable.add_column("Inches-Incline", justify="right", no_wrap=True)
+alltable.add_column("Cant Compensation", justify="right", no_wrap=True)
+for point in hold_overs.points:
+    incline_compensation = get_incline_compensation(point.path_inches, -15)
+
+    cant_compensation = get_cant_compensation(point.seconds, 90, 1.5)
+
+    alltable.add_row (point.yards, point.meters, str(point.path_inches), str(incline_compensation), str(abs(point.path_inches-incline_compensation)), str(cant_compensation))
+
+console.print(alltable)
+print()
+
+# Print Configuration Table
+conftable = Table(title="Configuration")
+conftable.add_column("Key", justify="left", no_wrap=True)
+conftable.add_column("Value", justify="left", no_wrap=True)
+for item in sorted(configuration):
+    conftable.add_row(item, str(configuration[item]))
+
+console.print(conftable)
+print()
+
+yardstable = Table(title="Whole Yards Only")
+yardstable.add_column("Yards", justify="right", no_wrap=True)
+yardstable.add_column("Drop - Inches", justify="right", no_wrap=True)
+yardstable.add_column("Drop - MOA", justify="right", no_wrap=True)
+yardstable.add_column("Drop - Mils", justify="right", no_wrap=True)
+yardstable.add_column("Time", justify="right", no_wrap=True)
+yardstable.add_column("Velocity", justify="right", no_wrap=True)
 for point in hold_overs.points:
     if float(point.yards).is_integer():
         pi = '{:.3f}'.format(round(point.path_inches, 3))
@@ -85,25 +82,36 @@ for point in hold_overs.points:
         milc = '{:.2f}'.format(round(point.mil_correction, 2))
         stime = '{:.3f}'.format(round(point.seconds, 3))
         vel = '{:.2f}'.format(round(point.velocity, 2))
-#        print("%8s | %8s  | %8s | %8s | %9s | %8s |" %
-#              (point.yards, pi, moac, milc, stime, vel))
-        table.add_row(point.yards, pi, moac, milc, stime, vel)
+        yardstable.add_row(point.yards, pi, moac, milc, stime, vel)
 
-console = Console()
-console.print(table)
+console.print(yardstable)
+print()
 
-#print()
-#print("Whole Meters Only")
-#print("%-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
-#      ("Range", "Drop", "MOA", "Mils", "Time", "Velocity"))
-#print("%-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
-#      ("(Meters)", "(Inches)", "", "", "(Seconds)", "(fps)"))
-#for point in hold_overs.points:
-#    if float(point.meters).is_integer():
-#        pi = '{:.3f}'.format(round(point.path_inches, 3))
-#        moac = '{:.2f}'.format(round(point.moa_correction, 2))
-#        milc = '{:.2f}'.format(round(point.mil_correction, 2))
-#        stime = '{:.3f}'.format(round(point.seconds, 3))
-#        vel = '{:.2f}'.format(round(point.velocity, 2))
-#        print("%8s | %8s  | %8s | %8s | %9s | %8s |" %
-#              (point.meters, pi, moac, milc, stime, vel))
+# Print Configuration Table
+conftable = Table(title="Configuration")
+conftable.add_column("Key", justify="left", no_wrap=True)
+conftable.add_column("Value", justify="left", no_wrap=True)
+for item in sorted(configuration):
+    conftable.add_row(item, str(configuration[item]))
+
+console.print(conftable)
+print()
+
+meterstable = Table(title="Whole Meters Only")
+meterstable.add_column("Meters", justify="right", no_wrap=True)
+meterstable.add_column("Drop - Inches", justify="right", no_wrap=True)
+meterstable.add_column("Drop - MOA", justify="right", no_wrap=True)
+meterstable.add_column("Drop - Mils", justify="right", no_wrap=True)
+meterstable.add_column("Time", justify="right", no_wrap=True)
+meterstable.add_column("Velocity", justify="right", no_wrap=True)
+for point in hold_overs.points:
+    if float(point.meters).is_integer():
+        if (float(point.meters) / 5).is_integer():
+            pi = '{:.3f}'.format(round(point.path_inches, 3))
+            moac = '{:.2f}'.format(round(point.moa_correction, 2))
+            milc = '{:.2f}'.format(round(point.mil_correction, 2))
+            stime = '{:.3f}'.format(round(point.seconds, 3))
+            vel = '{:.2f}'.format(round(point.velocity, 2))
+            meterstable.add_row(point.meters, pi, moac, milc, stime, vel)
+
+console.print(meterstable)
