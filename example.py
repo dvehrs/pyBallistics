a9668d14ec3eb0e1e4963151ddd1ee1cfcae42c0
lowkey <dvehrs@gmail.com>
2025-07-21 10:18:16 -0600

    Added combined meters and yards output table

--- example.py
+++ example.py
@@ -39,6 +39,9 @@ for point in hold_overs.points:
           (point.yards, point.meters, point.path_inches, incline_compensation, abs(point.path_inches-incline_compensation), cant_compensation))
 
 print()
+print("Human Readable Formated Output (Whole Yards, Whole Meters and Both)")
+
+print()
 print("Whole Yards Only")
 print("%-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
       ("Range", "Drop", "MOA", "Mils", "Time", "Velocity"))
@@ -69,3 +72,18 @@ for point in hold_overs.points:
         vel = '{:.2f}'.format(round(point.velocity, 2))
         print("%8s | %8s  | %8s | %8s | %9s | %8s |" %
               (point.meters, pi, moac, milc, stime, vel))
+
+print()
+print("All Distances (whole yards or meters)")
+print("%-8s | %-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
+      ("Range Y", "Range M", "Drop", "MOA", "Mils", "Time", "Velocity"))
+print("%-8s | %-8s | %-8s  | %-8s | %-8s | %-9s | %-8s |" %
+      ("(Yards)", "(Meters)", "(Inches)", "", "", "(Seconds)", "(fps)"))
+for point in hold_overs.points:
+    pi = '{:.3f}'.format(round(point.path_inches, 3))
+    moac = '{:.2f}'.format(round(point.moa_correction, 2))
+    milc = '{:.2f}'.format(round(point.mil_correction, 2))
+    stime = '{:.3f}'.format(round(point.seconds, 3))
+    vel = '{:.2f}'.format(round(point.velocity, 2))
+    print("%8s | %8s | %8s  | %8s | %8s | %9s | %8s |" %
+          (point.yards, point.meters, pi, moac, milc, stime, vel))
